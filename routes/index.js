927139e4bb70827c182494cfe17b3d5dcf87f0f2
Ashiq <ashiqm@amazon.com>
2017-07-26 13:04:28 +0530

    google form and member list route update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Onam2017@Amazon' });
 });
+
+router.get('/members', function(req, res, next) {
+  res.render('members', { title: 'Members' });
+});
+
+//https://docs.google.com/forms/d/19ODwZzPfb_yhcowcRot39lGfulFsh0i-SOfpn6afw14/edit#responses
 
 module.exports = router;
